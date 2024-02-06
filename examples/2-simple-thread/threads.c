6e9c8d1cfb479d2094d54337f31afcc0773d35ef
Jon Craton <jncraton@gmail.com>
2024-02-06 15:26:00 -0500

    Update example

--- examples/2-simple-thread/threads.c
+++ examples/2-simple-thread/threads.c
@@ -3,21 +3,21 @@
 #include <unistd.h>
 
 static void* child(void *ignored) {
-  sleep(3);
-  printf("Child is done sleeping for 3 seconds.\n");
+  sleep(1);
+  printf("Child is done sleeping for 1 second.\n");
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
   pthread_t child_thread;
   
-  int code;
-  code = pthread_create(&child_thread, NULL, child, NULL);
-  if (code) {
-    fprintf(stderr, "pthread_create failed with code %d\n", code);
+  int error;
+  error = pthread_create(&child_thread, NULL, child, NULL);
+  if (error) {
+    fprintf(stderr, "pthread_create failed with error %d\n", error);
   }
 
-  sleep(5);
-  printf("Parent is done sleeping for 5 seconds.\n");
+  sleep(3);
+  printf("Parent is done sleeping for 3 seconds.\n");
   return 0;
 }
