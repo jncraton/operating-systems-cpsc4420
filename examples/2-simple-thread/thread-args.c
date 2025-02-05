16c3f5c802c4f0eebc5feeb40acd61e7e1226c72
Jon Craton <jncraton@gmail.com>
2025-02-05 08:49:22 -0500

    Thread example

--- examples/2-simple-thread/thread-args.c
+++ examples/2-simple-thread/thread-args.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define SUM_TO 5
+#define SUM_TO 10
 
-static void* child(void * num) {
+static void* square(void * num) {
   long num_int = *(long*)num;
+
+  printf("Child thread computing square of %d\n", num_int);
 
   return (void*)(num_int * num_int);
 }
@@ -17,22 +19,21 @@ int main() {
   int error;
   for (int i = 1; i<=SUM_TO; i++) {
     inputs[i] = i;
-    error = pthread_create(&child_thread[i], NULL, child, &inputs[i]);
+    error = pthread_create(&child_thread[i], NULL, square, &inputs[i]);
     if (error) {
       fprintf(stderr, "pthread_create failed with error %d\n", error);
     }
   }
 
-  long sum = 0;
+  long long sum = 0;
   long squares[SUM_TO+1];
 
   for (int i = 1; i<=SUM_TO; i++) {
     pthread_join(child_thread[i], (void*)&squares[i]);
     sum += squares[i];
-    printf("%d %d\n", squares[i], sum);
   }
 
-  printf("Sum of squares from 1 to %d is %d.\n", SUM_TO, sum);
+  printf("Sum of squares from 1 to %d is %ld.\n", SUM_TO, sum);
   
   return 0;
 }
