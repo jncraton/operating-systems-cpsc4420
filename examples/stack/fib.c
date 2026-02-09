f403b2a30f59edfbeaacaee15c16c2e5cf72010d
Jon Craton <jncraton@gmail.com>
2026-02-09 12:57:00 -0500

    Add fib.c

---
+++ examples/stack/fib.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int fib(int n) {
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main(void) {
+    int n = 20;
+    printf("fib(%d) = %d\n", n, fib(n));
+    return 0;
+}
