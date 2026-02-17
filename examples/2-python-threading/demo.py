2473bc3d942943490729c2556c2526eb45da14ab
Jon Craton <jncraton@gmail.com>
2026-02-17 10:44:34 -0500

    Add demo.py

---
+++ examples/2-python-threading/demo.py
@@ -0,0 +1,44 @@
+import threading
+import json
+import time
+from urllib.request import urlopen, Request
+
+junk_foods = [
+  'Pizza',
+  'Popcorn',
+  'Hamburger',
+  'Pepsi',
+  'Potato_chips',
+  'Cake',
+]
+
+url = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='
+
+def get_desc(food):
+    url = f'https://en.wikipedia.org/w/api.php?action=parse&format=json&page={food}'
+    headers = {'User-Agent': 'jncraton-pthread-demo/1.0 (User:Jncraton)'}
+    req = Request(url, headers=headers)
+    contents = urlopen(req).read()
+    print(f"{food}: {json.loads(contents)['parse']['properties'][0]['*']}")
+
+t = time.time()
+
+for food in junk_foods:
+    get_desc(food)
+
+print("Single threaded time:" + str(time.time() - t))
+
+t = time.time()
+
+threads = []
+
+for food in junk_foods:
+     threads.append(threading.Thread(target=get_desc, args=(food,)))
+     threads[-1].start()
+
+for thread in threads:
+    thread.join()
+ 
+print("Multi threaded time:" + str(time.time() - t))
+
+exit()
