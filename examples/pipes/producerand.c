5fc7a259f98ddd8cf7e71f11ca60432da2f1ed97
Jon Craton <jncraton@gmail.com>
2026-03-30 12:37:27 -0400

    Add producerand.c

---
+++ examples/pipes/producerand.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void) {
+  while (1) {
+    int random_val = rand();
+    int delay = (rand() % 3) + 1;
+
+    printf("%d\n", random_val);
+    fflush(stdout);
+
+    sleep(delay);
+  }
+
+  return 0;
+}
